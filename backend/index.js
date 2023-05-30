a102eabf66011cf05e923d9f5d9384b152d6de15
Aryan Dev Shourie <78311368+Aryan2727-debug@users.noreply.github.com>
2023-05-31 00:33:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 var cors = require('cors');
 const dotenv = require("dotenv");
-const path = require("path");
 
 dotenv.config({path : './config.env'});
 
@@ -12,17 +11,6 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-app.use(express.static(path.join(__dirname, "./frontend/build")));
-
-app.get("*", function(req, res){
-  res.sendFile(
-    path.join(__dirname, "./frontend/build/index.html"),
-    function(err){
-      res.status(500).send(err);
-    }
-  );
-});
 
 // Available Routes
 app.use('/api/auth', require('./routes/auth'));
